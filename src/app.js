85a7c010dfde013e0bfdbd489f0b24864b4cf6b3
NamuraCode <dnaranjo0@misena.edu.co>
2021-10-24 19:20:34 -0500

    se creo login y register con su css

--- src/app.js
+++ src/app.js
@@ -13,6 +13,14 @@ app.get("/register",(req, res)=>{
     res.sendFile(path.resolve(__dirname,"./views/register.html"))
 })
 
+app.get("/home",(req, res)=>{
+    res.sendFile(path.resolve(__dirname,"./views/home.html"))
+})
+
+app.get("/login",(req, res)=>{
+    res.sendFile(path.resolve(__dirname,"./views/login.html"))
+})
+
 app.listen(3000,()=>{
     console.log('listening on')
 })
